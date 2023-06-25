8cc4d55ea1dd895d433d7f7a8e772cfa9608d21b
ansyrllah <ansyarullahgo@gmail.com>
2023-06-25 13:28:02 +0700

    perbaikan fitur admin: add new book

--- process.php
+++ process.php
@@ -51,7 +51,7 @@
 		}
 	}
 
-	// session_unset();
+	session_unset();
 ?>
 	<p class="lead text-success">Your order has been processed sucessfully. Please check your email to get your order confirmation and shipping detail!. 
 	Your cart has been empty.</p>
