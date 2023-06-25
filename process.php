e62ba990402e5af838b5691fcfdf0b08d4ea52eb
ansyrllah <ansyarullahgo@gmail.com>
2023-06-25 21:38:40 +0700

    rapihkan saja

--- process.php
+++ process.php
@@ -53,7 +53,7 @@
 
 	session_unset();
 ?>
-	<p class="lead text-success">Your order has been processed sucessfully. Please check your email to get your order confirmation and shipping detail!. 
+	<p class="lead text-success">Your order has been processed sucessfully!. 
 	Your cart has been empty.</p>
 
 <?php
